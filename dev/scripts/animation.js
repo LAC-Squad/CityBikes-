d31e112c3b211ce526b4cd48a23a0a2cf50b8332
Chiara Zielinsky <chiara.zielinsky@gmail.com>
2017-03-10 15:46:19 -0500

    added styling and animation

--- dev/scripts/animation.js
+++ dev/scripts/animation.js
@@ -1,29 +1,4 @@
-
-
-// function citybikes(){
-  
-// citybikes.setBackground = () => { // Used to randomly generate images in the background
-//     let numberofImages = 5;
-//       citybikes.backgroundImageArray = ["assets/bean.png", "assets/bike.png", "assets/coffeesleeve.png", "assets/happycup.png", "assets/pin.png"]
-
-//     const maxWindowWidth = (Math.max(document.documentElement.clientWidth, window.innerWidth || 0)) - 140; // Calculates max window width to put images in
-//     const maxWindowHeight = (Math.max(document.documentElement.clientHeight, window.innerHeight || 0))-140; // Calculates max window height to put images in
-    
-
-//     for(let i = 0; i <= numberofImages; i++) { // runs a loop to randomly generate 5 images into the background
-//         let randomPositionX = Math.floor((Math.random() * maxWindowWidth) + 1);
-//         let randomPositionY = Math.floor((Math.random() * maxWindowHeight) + 1);
-//         let randomImage = Math.floor((Math.random() * 151) + 1);
-//         let randomImageSize = Math.floor((Math.random() * 120) + 100);
-
-//         $("backgroundImages").append(`<img src="${backgroundImageArray}.png" class="images" "image${i}">`);
-//         $(`.image${i}`).height(randomImageSize).width(randomImageSize).css("position","fixed").css("top",randomPositionY).css("left",randomPositionX).css("z-index",-100);
-
-//     } 
-//   }  //End of for loop
-// };
-
-
+//function for background animations
 
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -32,10 +7,11 @@ function getRandomInt (min, max) {
 $.fn.randomOrder = function(animate) {
   this.each(function() {
     var image = $(this);
+    var fadeSpeed = 100;
     
     // Viewport Dimensions
-    var vpHeight = $(window).height();
-    var vpWidth = $(window).width();
+    var vpHeight = $('imgWrapper').height();
+    var vpWidth = $('imgWrapper').width();
     
     // Image Position
     var xPos = getRandomInt(0, vpWidth - image.width());
@@ -43,12 +19,21 @@ $.fn.randomOrder = function(animate) {
     var zIndex = getRandomInt(0,13);
     
     // Animation Duration
-    if(animate) var dur = 500;
+    if(fadeIn) var dur = 500;
     else var dur = 0;
     
+    image.animate{left: xPos, top: yPos, 'z-index': zIndex}, dur);
     image.animate({left: xPos, top: yPos, 'z-index': zIndex}, dur);
   });
+  });
+
 };
+$('img').fadeIn(600, function() {
+
+
+    
+
+
 
 //Setup
 $('img').randomOrder(false);
@@ -59,4 +44,7 @@ window.setInterval(function(){
   $('img').randomOrder(true);
 },10000);
 
+//
+//fade in 
+}
 

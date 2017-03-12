1f70780b2c8b46ef6cced2792faa39abea08044f
Aaron Roberts <Lumbearjack@gmail.com>
2017-03-11 19:18:33 -0500

    Got nearby bikes on map!

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -25,26 +25,9 @@ let citybikes = {};
 citybikes.googleMapsKey = "AIzaSyCrXerr7xHcFxr4OW7EmOdDnNBaHgLzXIQ";
 
 citybikes.init = function(){
-	citybikes.getBikeNetworks();
+	// citybikes.getBikeNetworks();
 	citybikes.getUserLocation();
 }
-
-// This checks the citybikes API, puts in the city name argument from our google location request as the endpoint
-citybikes.getBikeNetworksStations = function(city){
-	console.log("CITY IS:",city)
-	var bikeNetworksStations = $.ajax({
-		// url:`http://api.citybik.es/v2/networks/${city}`,
-		url:`http://api.citybik.es/v2/networks/toronto`,
-		method:'GET',
-		dataType: 'json',
-		data:{
-		}
-	});
-	$.when(bikeNetworksStations).done(function(result){
-		console.log("RESULT",result);
-	})
-}
-
 
 citybikes.getUserLocation = function(res) {
 	$('#searchForm').submit(function(event){
@@ -82,29 +65,30 @@ citybikes.getUserLocation = function(res) {
 		.then(function(bikedata,cafedata){
 			// console.log("DATAS",bikedata, cafedata)
 			// console.log("CITY?",bikedata[0].results[0].address_components[3].long_name)
-			citybikes.cityName1 = (bikedata[0].results[0].address_components[3].long_name);
+
+
 			console.log("DATAS",bikedata, cafedata)
 			console.log("CITY [3]?",bikedata[0].results[0].address_components[3].long_name)
 			console.log("CITY [4]???",bikedata[0].results[0].address_components[4].long_name)
 
+			// Get City Name
+			citybikes.cityName1 = (bikedata[0].results[0].address_components[3].long_name).toLowerCase();
+			citybikes.cityName1B = (bikedata[0].results[0].address_components[4].long_name).toLowerCase();
 
-			// Get Bikes
+			// Get Bike
 			citybikes.bikeLat = bikedata[0].results[0].geometry.location.lat;
 			citybikes.bikeLng = bikedata[0].results[0].geometry.location.lng;
-			// Get City Name
-			citybikes.cityName = bikedata[0].results[0].address_components[4].long_name;
-
 			// Get Cafe
 			citybikes.cafeLat = cafedata[0].results[0].geometry.location.lat;
 			citybikes.cafeLng = cafedata[0].results[0].geometry.location.lng;
 
 			citybikes.getBikeNetworksStations(citybikes.cityName1);
+			citybikes.getBikeNetworksStations(citybikes.cityName1B);
 
 			//map thing
 			// console.log("WORKS???",citybikes.bikeLat, citybikes.bikeLng,"|",citybikes.cafeLat, citybikes.cafeLng);
 			// citybikes.getBikeNetworks(citybikes.bikeLat,citybikes.bikeLng);
-			initMap();
-			initDM();
+
 
 
 			// console.log("WORKS???",citybikes.bikeLat, citybikes.bikeLng,"|",citybikes.cafeLat, citybikes.cafeLng);
@@ -113,26 +97,43 @@ citybikes.getUserLocation = function(res) {
 		// 
 	});	
 };
-
-citybikes.getBikeNetworks = function(){
-	var bikeNetworks = $.ajax({
-		url:'http://api.citybik.es/v2/networks',
+// This checks the citybikes API, puts in the city name argument from our google location request as the endpoint
+citybikes.getBikeNetworksStations = function(city){
+	console.log("CITY IS:",city)
+	var bikeNetworksStations = $.ajax({
+		url:`http://api.citybik.es/v2/networks/${city}`,
+		// url:`http://api.citybik.es/v2/networks/toronto`,
 		method:'GET',
 		dataType: 'json',
 		data:{
 		}
 	});
-	$.when(bikeNetworks).done(function(data){
-
-		// citybikes.cityName1 = bikedata[0].results[0].address_components[4].long_name;
-
-		citybikes.bikeStations = data.networks;
-		citybikes.stationLng = citybikes.bikeStations[0].location.longitude;
-		citybikes.stationLat = citybikes.bikeStations[0].location.latitude;
-		// console.log("bike stations", citybikes.bikeStations);
-
+	$.when(bikeNetworksStations).done(function(result){
+		console.log("RESULT",result);
+		citybikes.bikeStations = result.network.stations; //latitude and longitude are accessible from this point
+		console.log(citybikes.bikeStations);
+		initMap();
 	})
-};
+}
+
+// citybikes.getBikeNetworks = function(){
+// 	var bikeNetworks = $.ajax({
+// 		url:'http://api.citybik.es/v2/networks',
+// 		method:'GET',
+// 		dataType: 'json',
+// 		data:{
+// 		}
+// 	});
+// 	$.when(bikeNetworks).done(function(data){
+
+// 		// citybikes.cityName1 = bikedata[0].results[0].address_components[4].long_name;
+
+// 		// citybikes.stationLng = citybikes.bikeStations[0].location.longitude;
+// 		// citybikes.stationLat = citybikes.bikeStations[0].location.latitude;
+// 		// console.log("bike stations", citybikes.bikeStations);
+
+// 	})
+// };
 
 citybikes.getCoffeeShops = function (lat,lng) {
 	return $.ajax({
@@ -166,44 +167,27 @@ function initMap() {
     // console.log('initmap');
     citybikes.myLatLong = {lat: citybikes.bikeLat, lng: citybikes.bikeLng};
     citybikes.cafeLatLong = {lat: citybikes.cafeLat, lng: citybikes.cafeLng};
-
- //    var centerLat = (citybikes.bikeLat - (citybikes.bikeLat % citybikes.cafeLat));
- //    var centerLong = (citybikes.bikeLng - (citybikes.bikeLng % citybikes.cafeLng));
- //    var centerLatLong = {lat: centerLat, lng: centerLong};
- //    console.log("Hello", centerLat, centerLong);
- //    console.log("HELLOOOO", centerLatLong);
- //    map = new google.maps.Map(document.getElementById('map'), {
-	//     center: centerLatLong,
-	//     zoom: 14
-	// });
- //    var marker = new google.maps.Marker({
- //        position: myLatLong,
- //        map: map,
- //        title: citybikes.userInputBike
- //    });
 	// Two Maps Attempt
 	//Bike Map
-	var twoZoom = 18;
+	var twoZoom =16;
 	mapBike = new google.maps.Map(document.getElementById('mapBike'), {
 	    center: citybikes.myLatLong,
 	    zoom: twoZoom
 	});
-	// console.log(citybikes.bikeStations);
-	    var markerOne = new google.maps.Marker({
-		    position: citybikes.myLatLong,
-		    map: mapBike,
-		    title: citybikes.userInputBike,
-		    icon: 'icons/bikeMarker.svg'
-	    })
-	
+	console.log(citybikes.bikeStations);
+    var markerOne = new google.maps.Marker({
+	    position: citybikes.myLatLong,
+	    map: mapBike,
+	    title: citybikes.userInputBike,
+	    // icon: 'icons/bikeMarker.svg'
+    })
+	// For each bike station, place a marker
 	citybikes.bikeStations.forEach(function(item){
-
-		// console.log("????", item);
-	    var marker = new google.maps.Marker({
-	    	
+		console.log("ITEM:", item);
+	    var marker = new google.maps.Marker({	
 	        position: {
-	        	lat:item.location.latitude,
-	        	lng:item.location.longitude
+	        	lat:item.latitude,
+	        	lng:item.longitude
 	        },
 	        map: mapBike,
 	        title: citybikes.userInputBike,
